46182f1eb21c53f4baa69a59204b3456f8bcc915
Charles Prezalor <cprezalor@gmail.com>
2016-04-29 09:16:43 +0800

    Version 2.0
    -Removed v.1 Log Parse
    +Completed argparse implementation
    +changed "extract_headers"  return datatype from list to string
    +improved error handling around file use
    +properly closed files upon exit

--- LogParse2.py
+++ LogParse2.py
@@ -5,19 +5,34 @@ import argparse
 import re
 import sys
 
-incoming_log_name = "log.log"
-output_log_name = "log2.csv"
-optimized_log = "weblog2.csv"
+#Configure Parser
+parser = argparse.ArgumentParser(description='Converts Fortinet web logs into CSVs')
+parser.add_argument("-i", metavar='filename', help="input log file name")
+parser.add_argument("-o", metavar='filename', help="full output log in CSV format")
+parser.add_argument("-w", metavar='filename', help="Minimal set of information for Web Logging")
+args = parser.parse_args()
+
+incoming_log_name = args.i
+output_log_name = args.o
+optimized_log = args.w
 log_array = []
 linecount = 0
 
 #Open the files for access
+if ((args.o and args.w) and (args.o == args.w)):
+	print("Web and Output logs cannot be the same file.")
+	sys.exit(1)
+
 try:
-	input_log = open(incoming_log_name, 'r')
-	output_log = open(output_log_name, 'w')
-	web_log = open(optimized_log, 'w')
+	if args.i:
+		input_log = open(incoming_log_name, 'r')
+	if args.o:
+		output_log = open(output_log_name, 'w')
+	if args.w:
+		web_log = open(optimized_log, 'w')
 except Exception as e:
 	print(str(e))
+	sys.exit(1)
 
 
 
@@ -30,7 +45,16 @@ def extract_headers(log_string):
 			if c != '"':
 				clean_header += c
 		headers.append(clean_header)
-	return headers
+	#Converts headers to string for return
+	head_len = len(headers)
+	x = 0
+	headstring = ""
+	for head_item in headers:
+		headstring += head_item 
+		if (x < head_len):
+			headstring += ","
+		x += 1
+	return headstring
 
 def extract_data(log_string):
 	#extracts actual data from log strings
@@ -43,18 +67,62 @@ def extract_data(log_string):
 		log_data.append(clean_data)
 	return log_data
 
-def trim_string(input_string):
-	#trims the trailing "," at the end of a string and removes double-quotes
-	temp_string = ""
-	if input_string[(len(input_string) - 1)] == "," :
-		for c in range(0,(len(input_string) - 1)):
-			if input_string[c] != "\"":
-				temp_string += input_string[c]
-		return temp_string
-	else:
-		return input_string
+def write_full_log():
+	x=0
+	tot_entries = len(log_array)		
+	while x < tot_entries:
+		#setting up variables to avoid trailing comma
+		#z current number of elements processed in log array[x]
+		z = 0
+		tot_elements = len(log_array[x])
+		for y in log_array[x]:
+			z += 1
+			#writing data to file
+			try:
+				output_log.write(str(y))
+				if (z < tot_elements):
+					output_log.write(",")
+				else:
+					output_log.write("\n")
+			except Exception as e:
+				print(str(e))
+				print("Could not write to web log file " + args.o)
+				sys.exit(2)
+		x += 1
 
 
+def write_web_log_headers():
+	try:
+		web_log.write("Date,Time,SourceIP,URL,Action,Category\n")
+	except Exception as e:
+		print(str(e))
+
+def write_web_log(input_array):
+	try:
+		#Date
+		web_log.write(input_array[0] + ",")
+		#Time
+		web_log.write(input_array[1] + ",")
+		#Source IP
+		web_log.write(input_array[10] + ",")
+		#URL
+		web_log.write(input_array[17] + "://" + input_array[18] + input_array[22] + ",")
+		#Action
+		web_log.write(input_array[20] + ",")
+		#Category
+		web_log.write(input_array[29] + "\n")
+	except Exception as e:
+		print(str(e))
+		print("Could not write to web log file " + args.w)
+		sys.exit(2)
+
+
+#MAIN
+#write web log headers
+if args.w:
+	 write_web_log_headers()
+
+#Generate data array 
 for entry in input_log:
 	linecount += 1
 	if linecount == 1:
@@ -62,13 +130,24 @@ for entry in input_log:
 		log_array[0].append(extract_headers(entry))
 	log_array.append([])
 	log_array[linecount] = (extract_data(entry))
-	#output_log.write(log_entry + "\n")
+	#Fill Web Log with Data
+	if args.w:
+			write_web_log(log_array[linecount])
 
-r=0
-for l in log_array[1]:
-	print(str(r) + ".", l)
-	r += 1
+if args.o:
+	write_full_log()
 
 
 print("Conversion completed!")
 print(linecount, "lines processed")
+
+try:
+	if args.i:
+		input_log.close()
+	if args.o:
+		output_log.close()
+	if args.w:
+		web_log.close()
+except Exception as e:
+	print(str(e))
+	sys.exit(1)
